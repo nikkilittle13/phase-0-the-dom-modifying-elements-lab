76c0a603b739023ab0c6597b3e7ebbe8fb553d72
Nikki Little <nikkilittle13@gmail.com>
2023-10-03 19:06:31 -0400

    creates myfile.md

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-main.remove();
-const newHeader = document.createElement("h1");
-newHeader.id = 'victory';
-newHeader.innerHTML = 'Nikki is the champion';
 
+const mainElement = document.getElementById('main');
 
+if(mainElement) {
+    mainElement.remove();
+} else {
+    console.log("The 'main#main' element does not exist.");
+}
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Nikki is the champion';
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
